851a59bc99d2c3d41dba80eb3c5aedc1ada1ab05
Anthony Luis Hernández Lagos - Ceiba Software <anthony.hernandez@ceiba.com.co>
2019-05-22 16:59:52 -0500

    Funciona 3

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging, by } from 'protractor';
-
+import { browser, logging, by, element } from 'protractor';
 describe('workspace-project App', () => {
   let page: AppPage;
 
@@ -13,11 +12,27 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('Parqueadero Ceiba');
   });
 
+  it('Debe guardar el registro de un nuevo vehiculo parqueado', () => {
+    browser.get('http://localhost:4200/registration');
+    element(by.id('plate')).sendKeys('BBBBBB');
+    element(by.id('type')).click();
+    element(by.id('type')).sendKeys('MOTO');
+    element(by.id('register')).submit();
+    browser.sleep(1000);
+  });  
+
   it('Debe retornar los vehículos parqueados', () => {
     browser.get('http://localhost:4200/parked');
+    expect(element(by.id('parked')).all(by.css('tr')).isPresent());
+  });
 
-    browser.isElementPresent(by.id('parked'));
-
+  it('Debe Dar salida al Vehiculo registrado', () => {
+    browser.sleep(1000);
+    browser.get('http://localhost:4200/registration')
+    browser.sleep(1000);
+    browser.get('http://localhost:4200/parked');
+    element(by.id('checkout')).click();
+    expect(element(by.id('parked')).all(by.css('tr')).count()).toEqual(0);     
   });
 
   afterEach(async () => {
